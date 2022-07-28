0e58ebee06722b72dace9c5e782310c6e3055502
Erbol <a1darbekov640@gmail.com>
2022-07-28 13:12:12 +0300

    fix

---
+++ src/api/interiordoor/controllers/interiordoor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  interiordoor controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::interiordoor.interiordoor');
---
+++ src/api/interiordoor/routes/interiordoor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * interiordoor router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::interiordoor.interiordoor');
